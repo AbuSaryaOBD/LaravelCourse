330ad3ca982e32e694d73fc30d92e2bcfe0bc722
AbuSaryaObd <abukarmo194@gmail.com>
2019-10-12 16:30:13 +0300

    git110

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcom
+                    Welcome Welocme </br> Alright
                 </div>
 
                 <div class="links">
