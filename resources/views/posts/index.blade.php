86a661d8a2ebd8380fee191ed3c8a4cf174f3ca1
AbuSaryaOBD <abukarmo194@gmail.com>
2019-10-21 09:49:36 +0300

    End Section

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,15 +4,16 @@
 @section('content')
     <ul>
         @foreach ($posts as $post)
-
-            <li><a href="{{ route('posts.show',$post->id) }}" >{{ $post->title }}</a>
-                <div class="image-container">
-                    <img src="{{ $post->path }}" alt="" width="50%">
-                </div>
+            <li>
+                <details>
+                    <summary><a href="{{ route('posts.show',$post->id) }}" >{{ $post->title }}</a></summary>
+                    <div class="image-container">
+                        <img src="{{ $post->path }}" alt="Comming Soon !!!" width="50%">
+                    </div>
+                </details>
             </li>   
         @endforeach
     </ul>
-
 @endsection
 
 @section('footer')
